4c98d8d4a00d18764f6c5a8f47b7ad5767680d8d
Danilo Augusto <127322985+dandanilo2010@users.noreply.github.com>
2024-07-26 15:34:04 -0300

    Add files via upload

---
+++ src/components/Button/TopBackground/index.jsx
@@ -0,0 +1,16 @@
+import Users from '../../../assets/users.png'
+import { Background } from './styles'
+
+function TopBackground() {
+    
+
+
+    return(
+        <Background>
+        <img src={Users} />
+      </Background>
+    )
+}
+
+
+export default TopBackground
